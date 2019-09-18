6cf24a233e83eb2757eb75dc2f8fa57c93de5d6c
mattericbrown <matt.eric.brown@gmail.com>
2019-09-17 20:47:17 -0700

    sidenav for a vue app

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <SideBar></SideBar>
+  </div>
+</template>
+
+<script>
+import SideBar from './components/SideBar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    SideBar
+  },
+  mounted() {
+    const plugin = document.createElement("script");
+    plugin.setAttribute(
+      "src",
+      "./assets/js/leon.js"
+    );
+    plugin.async = true;
+    document.head.appendChild(plugin);
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Roboto', Helvetica, Arial, sans-serif;
+  font-size: 15px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #443e3c;
+  padding: 0;
+}
+</style>
